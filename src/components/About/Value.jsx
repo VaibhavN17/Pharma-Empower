f9ca8f47ce941ed44f850493ba7f982d89d2f825
massera-shaikh <maserashaikh4@gmail.com>
2025-12-21 15:42:05 +0530

    added value.css and value.jsx and created folder EmpoerTechAI

---
+++ src/components/About/Value.jsx
@@ -0,0 +1,15 @@
+import './Value.css'; 
+
+const Value = () => {
+  return (
+    <div className="values-section">
+      <h2>Our Values</h2>
+      <p>
+        Integrity, respect, open access, and a commitment to developing capable
+        professionals who drive progress in healthcare and society.
+      </p>
+    </div>
+  );
+};
+
+export default Value;
