78b43a466e99f9d272a9d2d23978ef43b7907348
saifghl <gholesaif1@gmail.com>
2025-12-22 13:20:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import PrivacyPolicy from './components/PrivacyPolicy';
 import About from './components/About/About';
 import EmpowerTechAI from './components/EmpowerTechAI/EmpowerTechAI';
 import Login from './components/Login/Login';
-import SkillBoard from './components/SkillBoard/SkillBoard';
+import SkillBoard from './components/EmpowerTechAI/skill';
 import EmergingTech from './components/EmpowerTechAI/emerging-tech';
 import AI from './components/EmpowerTechAI/AI';
 import './App.css';
