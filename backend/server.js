a415e9bf7eace91333182e1145ac7d12c5e43827
Nadim khan <khanm99098@gmail.com>
2025-12-26 11:37:50 +0530

    backend setup done

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const path = require('path');
+
+
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+
+
+// Import database
+const pool = require('./config/db');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+// Test database connection
+pool.getConnection()
+    .then(connection => {
+        console.log('Database connected successfully!');
+        connection.release();
+    })
+    .catch(err => {
+        console.error('Database connection failed:', err);
+    });
+
+
+// Routes
+
+app.use('/api/auth', authRoutes);
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
+
+
---
+++ src/services/api.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000';
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+// Add auth token to requests
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const authAPI = {
+    register: (userData) => api.post('/auth/register', userData),
+    login: (credentials) => api.post('/auth/login', credentials),
+    create: (data) => api.post("/enquiries", data)
+};
+
+export const videoAPI = {
+  getAll: () => api.get('/video'),
+  getAdd: (data) => api.post('/video',data),
+};
+
+// ---------------- ADMIN ----------------
+export const adminAPI = {
+  addPatient: (data) => api.post("/admin/addpatient", data),
+  sendNotification:(data)=> api.post("/admin/notification",data),
+  getNotifications:()=>api.get("/admin/notification")
+};
+
+// ----------- APPOINTMENT ----------------------
+
+export const appointmentAPI={
+   takeAppointment:(data)=> api.post("/appointment",data),
+   getAllAppointments:()=>api.get("/appointment"),
+   deleteAppointment: (id) => api.delete(`appointment/${id}`)
+}
+
+// --------- Chat APi --------------
+
+export const chatApi={
+   sendChat:(message)=>api.post("/chat",message),
+   getChat:()=>api.get("/chat")
+}
+
+
+// -----------Enquiry Api ---------------
+
+export const inquiryAPI={
+  createInquery:(data)=>api.post("/enquiries",data),
+  getAllEnquiries:()=>api.get("/enquiries")
+}
+
+
+
+export default api;
