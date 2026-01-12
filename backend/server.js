54bbb788c0508dd5bd74c60100e556df8f9229d7
Sheefa Shaikh <shaikhshifa15214@gmail.com>
2026-01-12 22:52:17 +0530

    Change server listen address to '0.0.0.0'

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,6 @@ app.use("/api/sessions", sessionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
